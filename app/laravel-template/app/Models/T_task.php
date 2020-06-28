ee2845f83d961a72e3e4a22453e0e99d03e7383d
Kage Soichiro <k4gepedia@gmail.com>
2020-06-28 15:41:47 +0900

    template add

---
+++ app/laravel-template/app/Models/T_task.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class T_task extends Model
+{
+    protected $table = 't_task';
+
+    const CREATED_AT = 'CREATED_AT';
+    const UPDATED_AT = 'UPDATE_AT';
+}
