448f602b4c20a584b49d057bf4d1afe7d848346b
Kage Soichiro <k4gepedia@gmail.com>
2020-07-14 00:08:33 +0900

    sample

--- app/system/app/Http/Controllers/Web/Top/SampleController.php
+++ app/system/app/Http/Controllers/Web/Top/SampleController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Web\Top;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Web\Controller;
 use Illuminate\Http\Request;
 
 class SampleController extends Controller
